cf8d2bd99ca99a37e4003f734d9cf492d1a0f148
Jim Seconde <jim.seconde@googlemail.com>
2016-08-31 22:44:26 +0100

    new route created, some controller code started

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\userBug;
 
 class DefaultController extends Controller
 {
@@ -16,6 +17,22 @@ class DefaultController extends Controller
     {
       return new Response (
         'JIM SECONDE\'S DOCTRINE SANDBOX'
+      );
+    }
+
+    /**
+     * @Route("/create", name="createBug")
+     */
+    public function createAction(Request $request)
+    {
+
+      $bug = new userBug;
+      $bug->setBugDescription('This is the inital bug to report from a controller');
+      $bug->setBugGuid(uniqid());
+      $bug->setBugDate(date('Y-m-d H:i:s'));
+
+      return new Response (
+        'SCRIPT TO CREATE BUG'
       );
     }
 }
